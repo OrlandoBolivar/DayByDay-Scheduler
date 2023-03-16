eed8301f44316c3f9cc6d64adb6b1749172515c5
OrlandoBolivar <123449213+OrlandoBolivar@users.noreply.github.com>
2023-03-16 21:07:53 +1100

    Readme.info

--- README.md
+++ README.md
@@ -1,1 +1,61 @@
-# DayByDay-Scheduler
+# **Day-By-Day-Scheduler**
+
+
+
+## Instructions
+ 
+After setting up a new repository in GitHub, please follow the steps below;
+
+in **Current time and date**,
+1. Calling dayjs() without parameters returns a fresh Day.js object with the current date and time.
+![current day variable](./assets/images/display%20time.jpg) 
+
+![value fisplay and format](./assets/images/1.jpg) 
+
+
+in **Children elements**,
+
+
+2. The localStorage object allows you to save key/value pairs in the browser using the following syntax:
+
+Window.localStorage or localStorage; this information is display in the console and the user will be able to see by commanding console.log (). Assig an unique id in the html file to linked the stored information to html elements.,
+
+
+![container.children](./assets/images/loops.jpg) 
+
+
+![Navigation elements added in the header linked to content](./assets/images/container.blocks.jpg) 
+
+
+in **conditions/loops of the function**,
+
+3. When activating the function through the container’s children the site will execute and waits for response first from the html before it runs CSS and Java.script
+
+![Navigation elements added in the header linked to content](./assets/images/conditions.jpg) 
+
+
+## Usage
+
+Each of the block on the singledaily scheduler has the option to sabe and storage information. 
+variable to take in consideration are:
+- Grey = Past
+- Red = Present
+- Green = Future
+
+once press the button at the end of the block, the information will be save and store in the local storage.
+
+On top of the page you will find the local current time adjusted on a 24hour format.
+
+## Contributing
+The code is opening to features without affecting the core elements on the main branch. 
+
+## Authors and acknowledgment
+```
+Bootcamp 1-1 tutor services, 
+
+```
+[w3schools](https://www.w3schools.com/jsref/prop_win_localstorage.asp)
+
+[day.js](https://day.js.org/docs/en/parse/now)
+
+[bootstrap](https://getbootstrap.com/)
--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
 var timeDisplayEl = $('#time-display');
 
-
-
 // display time 
 function displayTime() {
   var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
@@ -17,10 +15,10 @@ $(document).ready(function () {
       var text = $(this).parent().siblings(".description").val(); 
       var time = $(this).parent().parent().attr("id"); 
 
-      //set items in local storage.
-      localStorage.setItem(time, text);
+     
+      localStorage.setItem(time, text); //storage data when you activate tje SetItem function.
   })
-  //load any saved data from LocalStorage - do this for each hour created. Should follow html 24 hour to 12 hour conversion.
+  //call any data from the container parent *each hour in the calendar and as your preference 24hr day or 12hr.
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
@@ -34,16 +32,16 @@ $(document).ready(function () {
   $("#hour-19pm .description").val(localStorage.getItem("hour-19pm"));
 
   function hourTracker() {
-      //get current number of hours.
-      var currentHour = dayjs().hour(); // use of moment.js
+      // get current hours using dayjs
+      var currentHour = dayjs().hour(); 
 
-      // loop over time blocks
+      // loop over each children from the container parent (each hour in the dairy)
       $(".time-block").each(function () {
           var blockHour = parseInt($(this).attr("id").split("-")[1]);
           console.log( blockHour, currentHour)
           console.log('hour-1pm'.split('-'))
 
-          //check if we've moved past this time, click into css/html given classes of past, present, or future
+          //identify if the children has pass the current time then execute java and css accordingly
           if (blockHour < currentHour) {
               $(this).addClass("past");
               $(this).removeClass("future");
@@ -62,65 +60,4 @@ $(document).ready(function () {
       })
   }
   hourTracker()
-// // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// var today = new Date();
-//   var day = today.getDay();
-//   var daylist = ["Sunday","Monday","Tuesday","Wednesday ","Thursday","Friday","Saturday"];
-//   console.log("Today is : " + daylist[day] + ".");
-//   var hour = today.getHours();
-//   var minute = today.getMinutes();
-//   var second = today.getSeconds();
-//   var prepand = (hour >= 12)? " PM ":" AM ";
-//   hour = (hour >= 12)? hour - 12: hour;
-//   if (hour===0 && prepand===' PM ') 
-//   { 
-//   if (minute===0 && second===0)
-//   { 
-//   hour=12;s
-//   prepand=' Noon';
-//   } 
-//   else
-//   { 
-//   hour=12;
-//   prepand=' PM';
-//   } 
-//   } 
-//   if (hour===0 && prepand===' AM ') 
-//   { 
-//   if (minute===0 && second===0)
-//   { 
-//   hour=12;
-//   prepand=' Midnight';
-//   } 
-//   else
-//   { 
-//   hour=12;
-//   prepand=' AM';
-//   } 
-//   } 
-//   console.log(today);
-
-// console.log("Current Time : "+hour + prepand + " : " + minute + " : " + second);
-// // the code isn't run until the browser has finished rendering all the elements
-// // in the html.
-// $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
-// });
 })
